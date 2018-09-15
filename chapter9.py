dceb9943f06f84c5da38b7f6c1bbb927b2642db2
RamboSofter <31913139+RamboSofter@users.noreply.github.com>
2018-09-15 15:29:24 +0800

    第九章

--- chapter9.py
+++ chapter9.py
@@ -80,3 +80,59 @@ def uses_only(word,use):
         return True    
     elif count!=0:
         return False
+#参考答案
+def has_no_e(word):
+    for letter in word:
+        if letter =='e':
+            return False
+    return True
+def avoids(word,forbidden):
+    for letter in word:
+        if letter in forbidden:
+            return False
+    return True
+def uses_only(word, available):
+    for letter in word:
+        if letter not in available:
+            return False
+    return True
+#uses_all函数，接收一个单词以及由需要的字母组成的字符串，当单词中所有需要的字母都出现了至少一次时返回True
+def uses_all(word,required):
+    for letter in required:
+        if letter not in word:
+            return False
+    return True
+def uses_all(word,required):
+    return uses_only(required,word)
+#编辑一个is_abecedarian的函数，如果单词中的字母是按照字母表顺序排列的（两个重复字母也ok），则返回True
+def is_abecdarian(word):
+    previous=word[0]
+    for c in word:
+        if c<previous:
+            return False:
+        previous=c
+    return True
+def is_abecdarian1(word):
+    if len(word)<=1:
+        return True
+    if word[0]>word[1]:
+        return False
+    return is_abecdarian1(word[1:])
+def is_abecdarinan2(word):
+    i=0
+    while i<len(word)-1:
+        if word[i+1]<word[i]:
+            return False
+        i=i+1
+    return True
+#判断是否是回文
+def is_palindrome(word):
+    i=0
+    j=len(word)-1
+    
+    while i<j:
+        if word[i]!=word[j]:
+            return False
+        i=i+1
+        j=j-1
+    return True
